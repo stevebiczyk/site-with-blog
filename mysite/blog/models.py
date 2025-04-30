3bd3617c7ff7b8cf80f402eb52e86e531765d373
stevebiczyk <stevebiczyk@gmail.com>
2025-04-30 22:18:39 +0100

    created author snippet, defined many-many relationship for blog posts and authors

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,7 +3,9 @@ from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, InlinePanel
 from datetime import date
-from modelcluster.fields import ParentalKey
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from wagtail.snippets.models import register_snippet
+from django import forms
 
 
 class BlogIndexPage(Page):
@@ -22,14 +24,14 @@ class BlogPostPage(Page):
     """
     date = models.DateField("Post date", default=date.today)
     intro =  RichTextField(blank=True)
-    author = models.CharField(max_length=255)
+    authors = ParentalManyToManyField('blog.Author', blank=True)
     title = models.CharField(max_length=255)
     body = RichTextField(blank=True)
     
     content_panels = Page.content_panels + [InlinePanel('image_gallery', label="Image Gallery"),
         FieldPanel('date'),
         FieldPanel('intro'),
-        FieldPanel('author'),
+        FieldPanel('authors', widget=forms.CheckboxSelectMultiple),
         FieldPanel('title'),
         FieldPanel('body'),
     ]
@@ -47,3 +49,22 @@ class BlogPageImageGallery(Orderable):
     )
     caption = models.CharField(max_length=255, blank=True)
     panels = [FieldPanel('image'), FieldPanel('caption')]
+    
+@register_snippet
+class Author(models.Model):
+    """
+    A model that represents an author.
+    """
+    name = models.CharField(max_length=255)
+    author_image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('author_image'),
+    ]
+    def __str__(self):
+        return self.name
