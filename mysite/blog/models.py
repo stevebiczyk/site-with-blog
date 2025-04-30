14f839b3c1c85c31ca5e548760abf77798a30c67
stevebiczyk <stevebiczyk@gmail.com>
2025-04-30 21:53:25 +0100

    added inline panels for image gallery

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from datetime import date
+from modelcluster.fields import ParentalKey
 
 
 class BlogIndexPage(Page):
@@ -25,7 +26,24 @@ class BlogPostPage(Page):
     title = models.CharField(max_length=255)
     body = RichTextField(blank=True)
     
-    content_panels = Page.content_panels + [
+    content_panels = Page.content_panels + [InlinePanel('image_gallery', label="Image Gallery"),
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('author'),
         FieldPanel('title'),
         FieldPanel('body'),
     ]
+    
+class BlogPageImageGallery(Orderable):
+    page = ParentalKey(
+        BlogPostPage,
+        on_delete=models.CASCADE,
+        related_name='image_gallery',
+    )
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.CASCADE,
+        related_name='+',
+    )
+    caption = models.CharField(max_length=255, blank=True)
+    panels = [FieldPanel('image'), FieldPanel('caption')]
