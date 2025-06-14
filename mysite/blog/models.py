e430f629c58d32f3389504ee52d13b42c7f735d3
stevebiczyk <stevebiczyk@gmail.com>
2025-06-14 16:53:42 +0100

    added search feature to blog index page

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,6 +9,7 @@ from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.fields import RichTextField
 from wagtail.models import Orderable, Page
 from wagtail.snippets.models import register_snippet
+from wagtail.search import index
 
 
 class BlogIndexPage(Page):
@@ -57,6 +58,8 @@ class BlogPostPage(Page):
         FieldPanel('body'),
         FieldPanel('tags'),
     ]
+    
+    search_fields = Page.search_fields + [index.SearchField('intro'), index.SearchField('body'), index.FilterField('date')]
     
 class BlogPageImageGallery(Orderable):
     page = ParentalKey(
